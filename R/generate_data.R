827f66dac4625da62daeb1c7d7e18b4dbeeec77a
Stacy Cyrille <stacyrille@hotmail.fr>
2026-03-05 19:18:06 +0100

    Update generate_data.R

--- R/generate_data.R
+++ R/generate_data.R
@@ -67,7 +67,7 @@ simulate_dna_pathways <- function(n,
   X <- sweep(Z, 2, thresholds, "<") * 1L
   
   colnames(X) <- paste0("D", seq_len(p))
-  as.data.frame(X)
+  as.matrix(X)
 }
 
 #' Simulate GSVA-like RNA pathway scores
@@ -98,3 +98,84 @@ simulate_rna_pathways <- function(n,
   return(as.data.frame(scaled_scores))
 }
 
+#' 
+#' @param X matrix of covariates.
+#' @param lambda0 Constant baseline hazard in the additive hazards model.
+#' @param beta Additive regression coefficients.
+#' @param censor_max Maximum censoring. Independent censoring times are 
+#' generated as C \sim Uniform(0, censor_max).
+#' 
+#' @return A data.frame containing observed time, event indicator and 
+#' covariates.
+#' 
+#' @export
+simulate_aalen_time <- function(X, lambda0, beta, censor_max){
+  
+  n <- nrow(X)
+  
+  # Events times from exponential with rate lambdaX
+  lambdaX <- as.vector(lambda0 + X %*% beta) 
+  U <- runif(n)
+  T <- -log(U)/lambdaX
+  
+  # Generate final censoring times
+  C <- runif(n, 0, censor_max)
+  
+  time <- pmin(T, C)
+  event <- as.integer(T <= C)
+  
+  data.frame(time, event, X)
+}
+
+
+#' Calibrate
+#' @param n_cal Number of individuals used in the Monte Carlo calibration step.
+#' @param lambda0 Constant baseline hazard.
+#' @param beta Additive regression coefficients.
+#' @param prev Marginal prevalences of the binary covariates.
+#' @param target_cens Desired censoring proportion.
+#' @param seed Random seed used during the calibration procedure. 
+#' @param max_upper Upper bound used during the root-finding procedure 
+#' (`uniroot`) to locate the value of cmax. 
+#' 
+#' @return Single numerical value which corresponds to the calibrated parameter 
+#' cmax such that the expected censoring proportion is approximately equal to 
+#' target_cens. 
+#'
+#' @export
+calibrate_censor_max_aalen <- function(
+    n_cal = 200000,
+    lambda0 = 0.05, 
+    beta = c(0.06, 0.02, 0.005, 0),
+    prev = c(0.005, 0.02, 0.05, 0.20),
+    target_cens = 0.20, 
+    seed = 2026, 
+    max_upper = 1e6
+    ){
+  set.seed(seed)
+  stopifnot(length(beta)==4, length(prev)==4)
+  
+  # Simulate binary covariates 
+  X <- sapply(prev, function(prob) rbinom(n_cal, 1, prob))
+  
+  # Hazards 
+  lambdaX <- as.vector(lambda0 + X %*% beta)
+  
+  # Event times 
+  U <- runif(n_cal)
+  T <- -log(U)/lambdaX
+  
+  # Deterministic expected censoring rate for Uniform(0, cmax)
+  cens_rate_det <- function(cmax) mean(pmin(1, T / cmax))
+  f <- function(cmax) cens_rate_det(cmax) - target_cens
+  
+  lower <- 1e-8
+  upper <- 1
+  while(f(upper) > 0 && upper < max_upper) upper <- upper * 2
+  if (f(lower)* f(upper) > 0) stop("Could not bracket root; increase max_upper or check target_cens.")
+  
+  censor_max <- uniroot(f, interval = c(lower, upper))$root
+  censor_max
+}
+  
+  

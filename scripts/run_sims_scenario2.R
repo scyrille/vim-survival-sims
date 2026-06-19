1423480e43b33b44458341cf554c20375f06e6af
Stacy Cyrille <stacyrille@hotmail.fr>
2026-06-19 17:03:40 +0200

    Create run_sims_scenario2.R

---
+++ scripts/run_sims_scenario2.R
@@ -0,0 +1,71 @@
+
+library(here)
+library(tidyverse)
+library(MASS)
+library(survML)
+library(SuperLearner)
+library(ranger)
+library(xgboost)
+library(timereg)
+library(survivalSL)
+
+source(here::here("R", "generate_data.R"))
+source(here::here("R", "pred_generators.R"))
+source(here::here("R", "do_one.R"))
+
+# Calibration parameters 
+calibration_param <- readRDS(here::here("outputs","results",
+                                        "mc_calibration_param.rds"))
+
+# Number of Monte Carlo replications 
+# R <- 500
+R <- 2
+
+# Sample size 
+# n <- c(500,1000,1500)
+n <- 100
+
+fs::dir_create(here::here("outputs", "results", "scenario2"))
+
+c_max <- calibration_param$params_2$c_max
+tau <- calibration_param$params_2$tau
+
+nuisances <- c("stackG", "cox.aalen")
+
+param_grid <- expand.grid(
+  n = n,
+  nuisance = nuisances
+)
+
+seeds <- sample.int(1e9, nrow(param_grid))
+
+output <- purrr::pmap(
+  param_grid,
+  function(n, nuisance) {
+    
+    res <- replicate(
+      R,
+      do_one(
+        n = n,
+        scenario = "2",
+        c_max = c_max,
+        tau = tau,
+        nuisance = nuisance
+      ),
+      simplify = FALSE
+    )
+    
+    saveRDS(
+      res,
+      here::here("outputs","results","scenario2",
+                 paste0("sims_n", n, "_", nuisance,".rds"))
+    )
+    
+    res
+  }
+)
+
+# names(output) <- paste0(
+#   "n", param_grid$n,
+#   "_", param_grid$nuisance
+# )
